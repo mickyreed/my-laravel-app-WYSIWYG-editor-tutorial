3d09917af01314aef8800be1b196ee866a0fd7a5
Micky Reed <xcal18r@gmail.com>
2025-01-24 23:50:54 +0800

    feat(web.php) Added Routes for Submissions index

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('/form', [FormController::class, 'showForm'])->name('form.show');
 Route::post('/form', [FormController::class, 'submitForm'])->name('form.submit');
 Route::get('/', [StaticPageController::class, 'landingPage'])->name('landing');
 Route::get('/', [StaticPageController::class, 'landingPage'])->name('landing');
-
-
+Route::get('/submissions', [FormController::class, 'index'])->name('form.index')
+    
 //Route::get('/', function () {
 //    return view('welcome');
 //}) ;
